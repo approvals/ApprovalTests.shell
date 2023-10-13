3c9ffd565b53b74a3f525351afaa94bf0b12b390
Michael R. Wolf <MichaelRWolf@att.net>
2023-10-13 18:20:09 -0400

    ! r Remove duplicate (dead) code

--- bash/verify.sh
+++ bash/verify.sh
@@ -3,19 +3,20 @@
 default_diff_tool="code --diff"
 diff_tool=$default_diff_tool
 
-function main(){
-    while getopts ":r:t:d:" opt; do
-      case $opt in
+while getopts ":r:t:d:" opt; do
+    case $opt in
 	d) diff_tool=$OPTARG;;
 	r) received_text=$OPTARG;;
 	t) test_name=$OPTARG;;
 	\?) echo "Invalid option: -$OPTARG" >&2;;
-      esac
-    done
+    esac
+done
 
-    received="$test_name.received"
-    approved="$test_name.approved"
+received="$test_name.received"
+approved="$test_name.approved"
 
+
+function main(){
     if [ "$received_text" == "" ];
     then
 	cat - > "$received"
@@ -41,12 +42,20 @@ function compare_and_approve(){
     diff -q "$received" "$approved" > /dev/null \
 	&& (pass; rm "$received") \
 	|| (fail;
-	    if [ -e /dev/tty ]; then
-		$diff_tool "$received" "$approved" </dev/tty
-	    else
-		$diff_tool "$received" "$approved"
-	    fi;
+    if [ -e /dev/tty ]; then
+	$diff_tool "$received" "$approved" </dev/tty
+    else
+	$diff_tool "$received" "$approved"
+    fi;
 	    false)
 }
+
+
+function compare_and_approve(){
+    diff -q "$received" "$approved" > /dev/null \
+	&& (pass; rm "$received") \
+	|| (fail_and_diff)
+}
+
 
 main
