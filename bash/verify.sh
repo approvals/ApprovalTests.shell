14f3f2494601be0e17bc4659db820a3cd93e4465
Michael R. Wolf <MichaelRWolf@att.net>
2023-10-13 17:43:57 -0400

    ! r Extract function: main

--- bash/verify.sh
+++ bash/verify.sh
@@ -3,6 +3,7 @@
 default_diff_tool="code --diff"
 diff_tool=$default_diff_tool
 
+function main(){
 while getopts ":r:t:d:" opt; do
   case $opt in
     d) diff_tool=$OPTARG;;
@@ -33,3 +34,6 @@ diff -q "$received" "$approved" > /dev/null \
             $diff_tool "$received" "$approved"
         fi;
         false)
+}
+
+main
